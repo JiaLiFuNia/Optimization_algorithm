0d65fb7d91d089eaf62e458cba30b9519d86e4d8
Jialifuniya <woshixubohan04@163.com>
2024-10-29 18:48:14 +0800

    Refactor code and update plot saving filenames

--- WF/RungeKutter_Adams_PECE_PMCEMC.m
+++ WF/RungeKutter_Adams_PECE_PMCEMC.m
@@ -11,7 +11,7 @@ t = 0:h:1;
 true_un = ut(t)';
 
 %% 三阶
-three_un = ones(length(t),1);
+three_un = u0*ones(length(t),1);
 
 for k = 1:length(t)-1
     K1 = ftu(t(k),three_un(k));
@@ -21,18 +21,18 @@ for k = 1:length(t)-1
 end
 
 %% 四阶
-four_un = ones(length(t),1);
+four_un = u0*ones(length(t),1);
 
 for k = 1:length(t)-1
     K1 = ftu(t(k),four_un(k));
-    K2 = ftu(t(k)+1/3*h,four_un(k)+1/3*h*K1);
-    K3 = ftu(t(k)+2/3*h,four_un(k)-1/3*h*K1+h*K2);
-    K4 = ftu(t(k)+h,four_un(k)+h*K1-h*K2+h*K3);
-    four_un(k+1) = four_un(k) + 1/8*h*(K1+3*K2+3*K3+K4);
+    K2 = ftu(t(k)+1/2*h,four_un(k)+1/2*h*K1);
+    K3 = ftu(t(k)+1/2*h,four_un(k)+1/2*h*K2);
+    K4 = ftu(t(k)+h,four_un(k)+h*K3);
+    four_un(k+1) = four_un(k) + 1/6*h*(K1+2*K2+2*K3+K4);
 end
 
 %% Adams q=3
-adams_un = ones(length(t),1);
+adams_un = u0*ones(length(t),1);
 q = 3;
 adams_un(1:q+1) = four_un(1:q+1);
 
@@ -41,36 +41,41 @@ for k = q+1:length(t)-1
 end
 
 %% PECE
-pece_un = ones(length(t),1);
+pece_un = u0*ones(length(t),1);
 pece_un(1:4) = four_un(1:4);
 
 for k = 1:length(t)-4
     pece_un(k+4) = pece_un(k+3) + 1/24*h*(55*ftu(t(k+3),pece_un(k+3))-59*ftu(t(k+2),pece_un(k+2))+37*ftu(t(k+1),pece_un(k+1))-9*ftu(t(k),pece_un(k)));
     fe = ftu(t(k+4),pece_un(k+4));
     while 1
-        pece_un(k+4) = pece_un(k+3) + 1/24*h*(9*fe+19*ftu(t(k+3),pece_un(k+3))-5*ftu(t(k+2),pece_un(k+2))+ftu(t(k+1),pece_un(k+1)));
-        fe_new = ftu(t(k+4),pece_un(k+4));
-        if(abs(fe-fe_new) < 1e-6)
+        un4_new = pece_un(k+3) + 1/24*h*(9*fe+19*ftu(t(k+3),pece_un(k+3))-5*ftu(t(k+2),pece_un(k+2))+ftu(t(k+1),pece_un(k+1)));
+        if(abs(pece_un(k+4)-un4_new) < 1e-6)
             break;
         end
-        fe = fe_new;
+        pece_un(k+4) = un4_new;
     end
 end
 
 %% PMECME
-% pmecme_un = ones(length(t),1);
-% pmecme_un(1:4) = four_un(1:4);
-% 
-% for k = 1:length(t)-4
-%     pmecme_un(k+4) = pmecme_un(k+3) + 1/24*h*(55*ftu(t(k+3),pmecme_un(k+3))-59*ftu(t(k+2),pmecme_un(k+2))+37*ftu(t(k+1),pmecme_un(k+1))-9*ftu(t(k),pmecme_un(k)));
-%     um_3 = pmecme_un(k+3);
-%     while 1
-%         um = pmecme_un(k+4) + 251/270*(um_3-pmecme_un(k+3));
-%         if(abs(um-pmecme_un(k+4)) < 1e-6)
-%             break;
-%         end
-%     end
-% end
+pmecme_un = u0*ones(length(t),1);
+pmecme_un(1:4) = four_un(1:4);
+
+un_m = 0;
+for k = 1:length(t)-4
+    pmecme_un(k+4) = pmecme_un(k+3) + 1/24*h*(55*ftu(t(k+3),pmecme_un(k+3))-59*ftu(t(k+2),pmecme_un(k+2))+37*ftu(t(k+1),pmecme_un(k+1))-9*ftu(t(k),pmecme_un(k)));
+    un4 = pmecme_un(k+4);
+    while 1
+        un0_ = pmecme_un(k+4) + 251/270*un_m;
+        fu0_ = ftu(t(k+4),un0_);
+        un1 = pmecme_un(k+3) + 1/24*h*(9*fu0_+19*ftu(t(k+3),pmecme_un(k+3))-5*ftu(t(k+2),pmecme_un(k+2))+ftu(t(k+1),pmecme_un(k+1)));
+        pmecme_un(k+4) = un1 - 19/270*(un1-pmecme_un(k+4));
+        if(abs(un4-pmecme_un(k+4)) < 1e-6)
+            break;
+        end
+        un4 = pmecme_un(k+4);
+        un_m = un1 - pmecme_un(k+4);
+    end
+end
 
 %% figure
 close all;
@@ -84,6 +89,7 @@ xlabel('$t$','Interpreter','latex');
 yyaxis right
 plot(t,log10(abs(three_un-true_un)));
 ylim([-12,-2]);
+xticks(0:0.1:1);
 ylabel('$log_{10}(|u(t)-u_t|)$',"Interpreter","latex");
 xlabel('$t$','Interpreter','latex');
 legend({'数值解','解析解','误差'},'Location','southeast');
@@ -98,6 +104,7 @@ xlabel('$t$','Interpreter','latex');
 yyaxis right
 plot(t,log10(abs(four_un-true_un)));
 ylim([-12,-2]);
+xticks(0:0.1:1);
 ylabel('$log_{10}(|u(t)-u_t|)$',"Interpreter","latex");
 xlabel('$t$','Interpreter','latex');
 legend({'数值解','解析解','误差'},'Location','southeast');
@@ -112,6 +119,7 @@ xlabel('$t$','Interpreter','latex');
 yyaxis right
 plot(t,log10(abs(adams_un-true_un)));
 ylim([-12,-2]);
+xticks(0:0.1:1);
 ylabel('$log_{10}(|u(t)-u_t|)$',"Interpreter","latex");
 xlabel('$t$','Interpreter','latex');
 legend({'数值解','解析解','误差'},'Location','southeast');
@@ -126,32 +134,36 @@ xlabel('$t$','Interpreter','latex');
 yyaxis right
 plot(t,log10(abs(pece_un-true_un)));
 ylim([-12,-2]);
+xticks(0:0.1:1);
 ylabel('$log_{10}(|u(t)-u_t|)$',"Interpreter","latex");
 xlabel('$t$','Interpreter','latex');
 legend({'数值解','解析解','误差'},'Location','southeast');
-title('PECE 格式');
+title('PECE 模式');
 set(gca,'Position',[0.56,0.39,0.38,0.24],'YMinorTick','on');
 
 subplot(3,2,5);
 yyaxis left
-plot(t,three_un,'.-',t,true_un);
+plot(t,pmecme_un,'.-',t,true_un);
 ylabel('$u$',"Interpreter","latex");
 xlabel('$t$','Interpreter','latex');
 yyaxis right
-plot(t,log10(abs(three_un-true_un)));
+plot(t,log10(abs(pmecme_un-true_un)));
 ylim([-12,-2]);
+xticks(0:0.1:1);
 ylabel('$log_{10}(|u(t)-u_t|)$',"Interpreter","latex");
 xlabel('$t$','Interpreter','latex');
 legend({'数值解','解析解','误差'},'Location','southeast');
-title('PMCEMC 格式');
+title('PMCEMC 模式');
 set(gca,'Position',[0.06,0.06,0.38,0.24],'YMinorTick','on');
 
 subplot(3,2,6);
-plot(t,log10(abs([three_un,four_un,adams_un,pece_un]-true_un)));
+plot(t,log10(abs([three_un,four_un,adams_un,pece_un,pmecme_un]-true_un)));
 ylim([-12,-2]);
+xticks(0:0.1:1);
 ylabel('$log_{10}(|u(t)-u_t|)$',"Interpreter","latex");
 xlabel('$t$','Interpreter','latex');
-legend({'三阶RK','四阶RK','Adams','PECE'},'Location','southeast','NumColumns',2);
+legend({'三阶RK';'四阶RK';'Adams';'PECE';'PMCEMC'},'Location','southeast','NumColumns',3);
+title('五种方法的误差对比')
 set(gca,'Position',[0.56,0.06,0.38,0.24],'YMinorTick','on');
 
 saveas(figure(1), 'figure2', 'png');
